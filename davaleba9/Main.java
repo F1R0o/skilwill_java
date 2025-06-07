5cb4cd29b1e8009a8184c43dcac6049e3a0764d4
luka <102388689+F1R0o@users.noreply.github.com>
2025-06-08 02:15:35 +0400

    mecxre davaleba

--- davaleba9/Main.java
+++ davaleba9/Main.java
@@ -3,7 +3,7 @@ package davaleba9;
 public class Main {
     public static void main(String[] args){
         Car car = new Car("mersedes","w221",2020,4);
-        System.out.println(car.toString());
+        System.out.println(car);
 
 
         int x = stringToInteger("99");
